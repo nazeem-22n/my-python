b981216b3be503754f619263bf21930404147608
nazeem-22n <nazeemsmd8@gmail.com>
2025-11-11 14:11:09 +0530

    Create usingelisstatement.py

---
+++ usingelisstatement.py
@@ -0,0 +1,10 @@
+#using elis statement
+mark=int(input("enter the number:"))
+if mark>=90:
+  print("grade A")
+elif mark>=75:
+  print("grade B")
+elif mark==50:
+  print("grade C")
+else: 
+  print("fail")
