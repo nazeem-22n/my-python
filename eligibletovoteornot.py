cee700c23cd682ac67e71c374b6864983258cfc2
nazeem-22n <nazeemsmd8@gmail.com>
2025-11-11 14:10:25 +0530

    Create eligibletovoteornot.py

---
+++ eligibletovoteornot.py
@@ -0,0 +1,6 @@
+#to check whether a person is eligible to vote or not (age ≥ 18)
+age=int(input("enter your age"))
+if age >=18:
+  print("you are eligible to vote")
+else:
+  print("you are not eligible to vote")
