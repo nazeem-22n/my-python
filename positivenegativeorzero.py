a3eb412bc0e0cffad3b0d27a289bf7ace95268f9
nazeem-22n <nazeemsmd8@gmail.com>
2025-11-11 14:05:27 +0530

    Create positivenegativeorzero.py

---
+++ positivenegativeorzero.py
@@ -0,0 +1,8 @@
+#to check weather Python program to check whether a number is positive, negative, or zero.
+n=int(input("enter the number:"))
+if n > 0:
+   print("it is an positive number")
+elif n <0:
+   print("it is an negative number")
+else :
+   print("it is an zero")
