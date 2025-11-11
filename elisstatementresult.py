c9ffc36e9c41324cd5e162ff7c00b789c89e8512
nazeem-22n <nazeemsmd8@gmail.com>
2025-11-11 14:04:16 +0530

    Create elisstatementresult.py

---
+++ elisstatementresult.py
@@ -0,0 +1,12 @@
+#using elis statement
+mark=int(input("enter the number:"))
+if mark>=92:
+  print("excellent")
+elif mark>=75:
+  print("good")
+elif mark==60:
+  print("avarage")
+elif mark>=35:
+  print("just pass")
+else 
+  print("fail")
