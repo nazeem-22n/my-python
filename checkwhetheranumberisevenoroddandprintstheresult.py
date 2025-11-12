3e378bafce24e73ef49e514fa209ae38d238d1c5
nazeem-22n <nazeemsmd8@gmail.com>
2025-11-12 16:14:09 +0530

    Create checkwhetheranumberisevenoroddandprintstheresult.py

---
+++ checkwhetheranumberisevenoroddandprintstheresult.py
@@ -0,0 +1,7 @@
+#checks whether a number is even or odd and prints the result.
+def check_even_or_odd(b):
+  if b%2== 0:
+    print("even number")
+  else:
+    print("odd number")
+check_even_or_odd(20)
