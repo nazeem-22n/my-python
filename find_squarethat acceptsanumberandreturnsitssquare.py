8609582a536e7e871f26c9eee7fce70310a747c3
nazeem-22n <nazeemsmd8@gmail.com>
2025-11-12 16:01:01 +0530

    Update and rename twonumbersasargumentsandreturnstheirsum.py to find_squarethat acceptsanumberandreturnsitssquare.py

---
+++ find_squarethat acceptsanumberandreturnsitssquare.py
@@ -0,0 +1,4 @@
+#find_square() that accepts a number and returns its square.
+def square(a):
+  return a**2
+square(6)
--- twonumbersasargumentsandreturnstheirsum.py
+++
@@ -1,4 +0,0 @@
-#add_numbers() that takes two numbers as arguments and returns their sum.
-def square(a):
-  return a**2
-square(6)
