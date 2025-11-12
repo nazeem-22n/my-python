a37db803f17c5f40ed88a6e18c9a08980310e256
nazeem-22n <nazeemsmd8@gmail.com>
2025-11-12 10:17:46 +0530

    Update multiplicationtable.py

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -1,6 +1,4 @@
-# Multiplication Table
-number = int(input("Enter a number to see its multiplication table: "))
-
-print(f"Multiplication Table for {number}:")
-for i in range(1, 11): # Loop from 1 to 10
+# Multiplication Table in formated sring
+number = int(input("enter the number")
+for i in range(1,11):
     print(f"{number} x {i} = {number * i}")
