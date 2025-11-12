38b6ce129ba34796d78516eb462ba09ec9dbfb89
nazeem-22n <nazeemsmd8@gmail.com>
2025-11-12 21:26:50 +0530

    Create sum_of_n_numbers.py

---
+++ sum_of_n_numbers.py
@@ -0,0 +1,9 @@
+#sum_of_n_numbers
+def sum_of_n_numbers():
+    n = int(input("enter number "))
+    total = 0
+    for i in range(n):
+        number = float(input("Enter a number: "))
+        total = total + number
+    return total
+result = sum_of_n_numbers()
